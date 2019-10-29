499e8dbe60dd69b029065886ab7e72e52036e5b9
Shiraj Sayed <shirajsayed13@gmail.com>
2019-10-29 20:00:42 +0530

    Add navigation graph new jetPack component and handle it

--- app/src/main/java/com/shirajsayed/wanttolearndagger2/ui/main/MainActivity.java
+++ app/src/main/java/com/shirajsayed/wanttolearndagger2/ui/main/MainActivity.java
@@ -6,25 +6,38 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.navigation.NavigationView;
 import com.shirajsayed.wanttolearndagger2.BaseActivity;
 import com.shirajsayed.wanttolearndagger2.R;
-import com.shirajsayed.wanttolearndagger2.ui.main.posts.PostsFragment;
 
-public class MainActivity extends BaseActivity {
+import java.util.Objects;
 
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         testFragment();
     }
 
     private void testFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container, new PostsFragment())
-                .commit();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -40,7 +53,43 @@ public class MainActivity extends BaseActivity {
             case R.id.logout:
                 sessionManager.logOut();
                 return true;
+            case android.R.id.home:
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                } else return false;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.main, true)
+                        .build();
+                Navigation.findNavController(this, R.id.nav_host_fragment)
+                        .navigate(R.id.profileScreen, null, navOptions);
+                break;
+            case R.id.nav_posts:
+                if (isValidDestination()) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsScreen);
+                }
+                break;
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    private boolean isValidDestination() {
+        return R.id.postsScreen != Objects.requireNonNull(Navigation
+                .findNavController(this, R.id.nav_host_fragment).getCurrentDestination()).getId();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout);
     }
 }
